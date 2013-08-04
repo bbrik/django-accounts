f61c07f7050564e521f64697e03aa153cfabfceb
Bernardo Brik <bernardobrik@gmail.com>
2013-08-03 22:38:31 -0300

    Altera view de login para usar template base dinâmico, definido nos settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+
+
+ACCOUNTS_BASE_TEMPLATE = getattr(settings, 'ACCOUNTS_BASE_TEMPLATE', 'base.html')
 
 
 def login(request):
-    return auth_views.login(request)
+    extra_context = {
+        'accounts_base': ACCOUNTS_BASE_TEMPLATE,
+    }
+    return auth_views.login(request, extra_context=extra_context)
