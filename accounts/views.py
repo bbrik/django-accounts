c73e226d1ac4c547f6ff83e6064686c9ffd24bd2
Bernardo Brik <bernardobrik@gmail.com>
2013-08-04 09:55:25 -0300

    move mensagens das views para dicionário no início do arquivo

--- accounts/views.py
+++ accounts/views.py
@@ -16,6 +16,13 @@ LOGOUT_REDIRECT_URL = resolve_url_for_config('LOGOUT_REDIRECT_URL') or '/'
 PASSWORD_CHANGE_REDIRECT_URL = resolve_url_for_config('PASSWORD_CHANGE_REDIRECT_URL') or '/'
 PASSWORD_RESET_REDIRECT_URL = resolve_url_for_config('PASSWORD_RESET_REDIRECT_URL') or '/'
 
+MESSAGES = {
+    'password_change': _(u'Senha alterada com sucesso.'),
+    'password_reset': _(u'Um email foi enviado para sua conta '
+                        u'com instruções de como recuperar sua senha.'),
+    'password_reset_confirm': _(u'Senha redefinida com sucesso.'),
+}
+
 
 def login(request):
     return auth_views.login(request, extra_context=EXTRA_CONTEXT)
@@ -26,8 +33,7 @@ def logout(request):
                              next_page=LOGOUT_REDIRECT_URL)
 
 
-@msg_success_redirect(PASSWORD_CHANGE_REDIRECT_URL, 
-                      _(u'Senha alterada com sucesso.'))
+@msg_success_redirect(PASSWORD_CHANGE_REDIRECT_URL, MESSAGES['password_change'])
 def password_change(request):
     return auth_views.password_change(
         request,
@@ -36,9 +42,7 @@ def password_change(request):
     )
 
 
-@msg_success_redirect(PASSWORD_RESET_REDIRECT_URL,
-                      _(u'Um email foi enviado para sua conta '
-                        u'com instruções de como recuperar sua senha.'))
+@msg_success_redirect(PASSWORD_RESET_REDIRECT_URL, MESSAGES['password_reset'])
 def password_reset(request):
     return auth_views.password_reset(
         request,
@@ -47,8 +51,7 @@ def password_reset(request):
     )
 
 
-@msg_success_redirect(PASSWORD_RESET_REDIRECT_URL,
-                      _(u'Senha redefinida com sucesso.'))
+@msg_success_redirect(PASSWORD_RESET_REDIRECT_URL, MESSAGES['password_reset_confirm'])
 def password_reset_confirm(request, uidb36, token):
     return auth_views.password_reset_confirm(
         request,
