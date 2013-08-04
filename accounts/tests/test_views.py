517842414eeebc5376f12e4cf8beb18612a16589
Bernardo Brik <bernardobrik@gmail.com>
2013-08-04 11:55:39 -0300

    adiciona teste para redirect

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from urlparse import urlparse
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+
+from ..base import LOGOUT_REDIRECT_URL
 
 
 class AccountsViewsTestCase(TestCase):
@@ -15,6 +19,11 @@ class AccountsViewsTestCase(TestCase):
     def check_no_user_logged_in(self):
         self.assertNotIn('_auth_user_id', self.client.session)
 
+    def check_redirect_to_url(self, response, url):
+        self.assertEqual(response.status_code, 302)
+        path = urlparse(response['location']).path
+        self.assertEqual(path, url)
+
     def test_login(self):
         url = reverse('accounts:login')
         self.check_no_user_logged_in()
@@ -25,5 +34,6 @@ class AccountsViewsTestCase(TestCase):
         url = reverse('accounts:logout')
         self.client.login(username='steve', password='12345')
         self.check_user_logged_in()
-        self.client.get(url)
+        response = self.client.get(url)
         self.check_no_user_logged_in()
+        self.check_redirect_to_url(response, LOGOUT_REDIRECT_URL)
