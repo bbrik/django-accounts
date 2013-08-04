68b5a5a1655f72efcac50420acf59bf3bf3fc3ee
Bernardo Brik <bernardobrik@gmail.com>
2013-08-03 23:46:49 -0300

    cria tag para form non field errors

--- accounts/templatetags/accounts_tags.py
+++ accounts/templatetags/accounts_tags.py
@@ -11,3 +11,10 @@ def bootstrap_field(field):
     return {
         'field': field,
     }
+
+
+@register.inclusion_tag('accounts/templatetags/bootstrap_non_field_errors.html')
+def bootstrap_non_field_errors(form):
+    return {
+        'form': form,
+    }
