ac9c5869feba1739d1dc3a119aada78aa8a4d6f9
Bernardo Brik <bernardobrik@gmail.com>
2013-08-06 22:40:25 -0300

    adiciona testes para password reset

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import re
 from urlparse import urlparse
 
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.core import mail
 
-from ..base import LOGOUT_REDIRECT_URL, PASSWORD_CHANGE_REDIRECT_URL
+from ..base import LOGOUT_REDIRECT_URL, PASSWORD_CHANGE_REDIRECT_URL, \
+    PASSWORD_RESET_REDIRECT_URL
+
+
+URL_RE = re.compile(r"""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.‌​][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(‌​([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))""", re.DOTALL)
 
 
 class AccountsViewsTestCase(TestCase):
@@ -49,3 +55,21 @@ class AccountsViewsTestCase(TestCase):
         self.check_redirect_to_url(response, PASSWORD_CHANGE_REDIRECT_URL)
         self.client.logout()
         self.client.login(username='steve', password='67890')
+
+    def test_password_reset(self):
+        url = reverse('accounts:password_reset')
+        response = self.client.post(url, data={'email': 'steve@gmail.com'})
+        self.check_redirect_to_url(response, PASSWORD_RESET_REDIRECT_URL)
+        self.assertEquals(len(mail.outbox), 1)
+        # acha url no texto da mensagem
+        message = mail.outbox[0]
+        match = URL_RE.search(message.body)
+        url = match.group(0)
+        path = urlparse(url).path[:-1] # remove aspas do final da url
+        # testa redefinição de senha
+        response = self.client.post(path, data={'new_password1': '67890',
+                                                'new_password2': '67890'})
+        self.check_redirect_to_url(response, PASSWORD_RESET_REDIRECT_URL)
+        self.check_no_user_logged_in()
+        self.client.login(username='steve', password='67890')
+        self.check_user_logged_in()
--- accounts_proj/settings/test.py
+++ accounts_proj/settings/test.py
@@ -19,3 +19,4 @@ DATABASES = {
     },
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
