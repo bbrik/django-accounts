c11250e48c0779b53bbcaea21b6b34450de23540
Bernardo Brik <bernardobrik@gmail.com>
2013-08-10 19:40:20 -0300

    melhora sintaxe de resolve_url_for_config

--- accounts/base.py
+++ accounts/base.py
@@ -6,17 +6,17 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 
-def resolve_url_for_config(name):
+def resolve_url_for_config(name, default=None):
     url = getattr(settings, name, None)
     if url is not None:
         return resolve_url(url)
-    return None
+    return default
 
 
 ACCOUNTS_BASE_TEMPLATE = getattr(settings, 'ACCOUNTS_BASE_TEMPLATE', 'base.html')
-LOGOUT_REDIRECT_URL = resolve_url_for_config('LOGOUT_REDIRECT_URL') or '/'
-PASSWORD_CHANGE_REDIRECT_URL = resolve_url_for_config('PASSWORD_CHANGE_REDIRECT_URL') or '/'
-PASSWORD_RESET_REDIRECT_URL = resolve_url_for_config('PASSWORD_RESET_REDIRECT_URL') or '/'
+LOGOUT_REDIRECT_URL = resolve_url_for_config('LOGOUT_REDIRECT_URL', '/')
+PASSWORD_CHANGE_REDIRECT_URL = resolve_url_for_config('PASSWORD_CHANGE_REDIRECT_URL', '/')
+PASSWORD_RESET_REDIRECT_URL = resolve_url_for_config('PASSWORD_RESET_REDIRECT_URL', '/')
 
 
 def msg_success_redirect(success_url, msg):
