f1ca0f11a27ec0e29108befbab3ef3cbdd92c913
Bernardo Brik <bernardobrik@gmail.com>
2013-08-13 11:54:09 -0300

    remove tudo que era do projeto de teste

--- accounts_proj/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, include, url
-
-
-urlpatterns = patterns('',
-    url(r'^', include('home.urls', namespace='home')),
-    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-)
